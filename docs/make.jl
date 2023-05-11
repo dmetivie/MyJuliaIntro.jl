a6abee65230e1c7165a48e9c2fec835a957683bc
David Métivier <46794064+dmetivie@users.noreply.github.com>
2023-05-11 13:28:57 +0200

    initial commit

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+# From https://github.com/KristofferC/DigiWellSeminarieJulia/blob/master/MyPackage/docs/make.jl
+using Documenter, MyJuliaIntro
+
+const liveserver = "liveserver" in ARGS
+
+if liveserver
+    using Revise
+    Revise.revise()
+end
+
+makedocs(
+    sitename = "My Julia Intro",
+    pages = Any[
+        "First things first" => "index.md",
+        "Get Started" => "GetStarted.md",
+        "Get Help" => "FAQ.md",
+        "Get Packages" => "packages.md"
+    ]
+)
+
+deploydocs(
+    repo = "https://dmetivie.github.io/MyJuliaIntro.jl/",
+    versions = nothing
+)
