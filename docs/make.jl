e8c07e89ce5da6dbd9c9f78cba1caaacf88ad9c1
David Métivier <46794064+dmetivie@users.noreply.github.com>
2023-05-12 11:07:25 +0200

    refactor doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,36 +1,18 @@
-# From https://github.com/KristofferC/DigiWellSeminarieJulia/blob/master/MyPackage/docs/make.jl
-import Pkg
-Pkg.add("Documenter")
 using Documenter
-push!(LOAD_PATH,"../")
-push!(LOAD_PATH,"../src/")
 using MyJuliaIntro
 
-const liveserver = "liveserver" in ARGS
-
-if liveserver
-    using Revise
-    Revise.revise()
-end
-
+push!(LOAD_PATH,"../src/")
 makedocs(
-    sitename = "My Introduction to Julia",
-    authors = "David Métivier",
-    pages = Any[
-        "First things first" => "index.md",
-        "Get Started" => "GetStarted.md",
-        "Get Help" => "FAQ.md",
-        "Get Packages" => "packages.md"
-    ]
+    sitename = "MyJuliaIntro",
+    pages = [
+        "First things first" => "index.md"
+    ],
+    format = Documenter.HTML(prettyurls = false),
 )
 
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
 deploydocs(
-    repo = "https://dmetivie.github.io/MyJuliaIntro.jl",
-    target = "build",
-    branch = "gh-pages",
-    versions = ["stable" => "v^", "v#.#" ],
+    repo = "github.com/dmetivie/MyJuliaIntro.jl.git"
 )
-
-# deploydocs(
-#     repo = "https://dmetivie.github.io/MyJuliaIntro.jl/",
-# )
